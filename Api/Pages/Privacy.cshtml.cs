0e2dc96b1d067230049730bfa6e4b2c785607efb
MuhammedSabri-04 <muhammedsabrisk04@gmail.com>
2026-02-06 22:25:59 +0300

    Reorganize project

---
+++ Api/Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Api.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
